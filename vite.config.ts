4c632ad89182cce90b6f995343d77a20442f05dd
David Andersen <git@davidandersen.us>
2025-05-30 03:04:02 +0000

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools()],
+  base: process.env.NODE_ENV === 'production' ? process.env.REPO_NAME || '/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
